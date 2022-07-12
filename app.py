4cd686e3fe5f6b9938e5d07ee0e08463f89b53ab
Sam Hardiment <samuelghardiment@gmail.com>
2022-07-12 19:16:07 +0100

    add setup for connectng sql db to heroku

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+import os
+
+
+##  Code block to connect sql db to heroku
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+CORS(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
