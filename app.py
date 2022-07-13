7f80be3b832f4c675b3cea8ea329fad0d8a5cfd7
Sam Hardiment <samuelghardiment@gmail.com>
2022-07-13 09:27:35 +0100

    first commit of day3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
